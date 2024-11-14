30130c8a1f367dedf7c492618ae49da41fb179a9
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-11-14 12:07:19 +0530

    Update Sum_Of_Digit

--- Sum_Of_Digits.c
+++ Sum_Of_Digits.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<conio.h>
 
-void main(){
+ 
     // Write a program to print the sum of digit a given number.
 
 int main() {
@@ -11,13 +11,11 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    // Use absolute value for negative numbers
-   // number = (number < 0) ? -number : number;
-
+     
     while (number > 0) {
-        digits = number % 10;  // Get the last digit
-        sum += digits;         // Add it to sum
-        number /= 10;          // Remove the last digit
+        digits = number % 10;   
+        sum += digits;         
+        number /= 10;           
     }
 
     printf("Sum of digits: %d\n", sum);
